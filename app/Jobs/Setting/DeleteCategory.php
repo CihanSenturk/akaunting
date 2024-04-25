86aea09009a435d67e0418360f1eccd07891d599
Cihan Şentürk <53110792+CihanSenturk@users.noreply.github.com>
2024-04-25 17:48:38 +0300

    fixed sub category delete issue

--- app/Jobs/Setting/DeleteCategory.php
+++ app/Jobs/Setting/DeleteCategory.php
@@ -3,16 +3,26 @@
 namespace App\Jobs\Setting;
 
 use App\Abstracts\Job;
+use App\Models\Setting\Category;
+use App\Interfaces\Job\ShouldDelete;
 use App\Events\Setting\CategoryDeleted;
 use App\Events\Setting\CategoryDeleting;
 use App\Exceptions\Settings\LastCategoryDelete;
-use App\Interfaces\Job\ShouldDelete;
-use App\Models\Setting\Category;
 
 class DeleteCategory extends Job implements ShouldDelete
 {
     public function handle(): bool
-    {
+    {   
+        foreach ($this->model->sub_categories as $sub_category) {
+            try {
+                $this->ajaxDispatch(new DeleteCategory($sub_category));
+            } catch (\Exception $e) {
+                flash($e->getMessage())->error()->important();
+            }
+        }
+
+        $this->model->refresh();
+
         $this->authorize();
 
         event(new CategoryDeleting($this->model));
@@ -39,7 +49,7 @@ class DeleteCategory extends Job implements ShouldDelete
         }
 
         // Can not delete the last category by type
-        if (Category::where('type', $this->model->type)->count() == 1) {
+        if (Category::where('type', $this->model->type)->count() == 1 && $this->model->parent_id === null) {
             $message = trans('messages.error.last_category', ['type' => strtolower(trans_choice('general.' . $this->model->type . 's', 1))]);
 
             throw new LastCategoryDelete($message);
@@ -50,10 +60,35 @@ class DeleteCategory extends Job implements ShouldDelete
 
             throw new \Exception($message);
         }
+
+        if ($this->model->sub_categories->count() >= 1) {
+            $relationships = [];
+
+            foreach ($this->model->sub_categories as $sub_category) {
+                $relationships[] = $this->getRelationships($sub_category);
+            }
+
+            $text = null;
+
+            $relationships = array_map('unserialize', array_unique(array_map('serialize', $relationships)));
+
+            foreach ($relationships as $relationship) {
+                $text = ($text ? $text . ', ' : $text) . implode(', ', $relationship);
+            }
+
+            $message = trans('messages.warning.deleted', ['name' => $this->model->name, 'text' => $text]);
+
+
+            throw new \Exception($message);
+        }
     }
 
-    public function getRelationships(): array
+    public function getRelationships($model = null): array
     {
+        if (! $model) {
+            $model = $this->model;
+        } 
+
         $rels = [
             'items' => 'items',
             'invoices' => 'invoices',
@@ -61,13 +96,17 @@ class DeleteCategory extends Job implements ShouldDelete
             'transactions' => 'transactions',
         ];
 
-        $relationships = $this->countRelationships($this->model, $rels);
+        $relationships = $this->countRelationships($model, $rels);
 
-        if ($this->model->id == setting('default.income_category')) {
+        foreach ($model->sub_categories as $sub_category) {
+            $this->countRelationships($sub_category, $rels);
+        }
+
+        if ($model->id == setting('default.income_category')) {
             $relationships[] = strtolower(trans_choice('general.incomes', 1));
         }
 
-        if ($this->model->id == setting('default.expense_category')) {
+        if ($model->id == setting('default.expense_category')) {
             $relationships[] = strtolower(trans_choice('general.expenses', 1));
         }
 
